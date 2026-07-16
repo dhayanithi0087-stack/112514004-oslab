d92301d6981c7349aa770decad2fc10a44d29288
dhayanithi0087-stack <dhayanithi0087@gmail.com>
2026-07-16 07:46:43 +0000

    ex1and 3

---
+++ oslab/exp3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<dirent.h>
+#include<string.h>
+int main()
+{
+    int ft;
+    char buffer[100]
+    struct stat fileInfo;
+    DIR *dir;
+    struct 
+}
